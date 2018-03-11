72aa16bd507812061931257e0bb1bb5eee12a072
Jack Vine <jackv949@gmail.com>
2018-03-11 16:21:22 +1030

    Added ai controller class

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -17,5 +17,5 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 public:
 	virtual void BeginPlay() override;
 
-	ATank * GetControlledTank() const;
+	ATank* GetControlledTank() const;
 };
